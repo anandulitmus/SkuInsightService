919246fa07d374eea5289ff925f7e0b18bda86f1
LITMUS7\s.anandu <s.anandu@litmus7.com>
2025-11-21 17:34:14 +0530

    Initial commit

---
+++ src/main/java/com/sephora/sku_aggregator_service/SkuAggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sephora.sku_aggregator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkuAggregatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkuAggregatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sephora/sku_aggregator_service/SkuAggregatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sephora.sku_aggregator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkuAggregatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
